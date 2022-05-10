572f4c592b61d9c92a35315e6b571be122ff95f7
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-10 21:32:05 +0600

    initialal set  , navbar and banner added

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+
+
+function App() {
+  return (
+    <div>
+
+
+      <Navbar></Navbar>
+
+
+      <Routes>
+
+        <Route path="/" element={<Home></Home>}> </Route>
+        <Route path='/about' element={<About></About>}> </Route>
+
+
+
+      </Routes>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+
+            <h2>This login</h2>
+
+        </div>
+    );
+};
+
+export default Login;
