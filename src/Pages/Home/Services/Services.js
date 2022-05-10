dd8e52348fc18243feb4f782f9c6b3bc99086317
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-11 01:36:06 +0600

    services added

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+
+            <h3 className='font-bold text-center text-secondary'>OUR SERVICES</h3>
+            <h1 className='text-center text-3xl'>Services We Provide</h1>
+
+
+
+        </div>
+    );
+};
+
+export default Services;
