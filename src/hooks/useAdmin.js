5cda36bc8fc7cd5ea52add3dca9aca091d3f3a92
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-07 22:45:59 +0600

    readme file edited

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -6,7 +6,29 @@ const useAdmin = (user) => {
     const [adminLoading, setAdminLoading] = useState(true)
 
 
+    useEffect(() => {
 
+        const email = user?.email
+
+
+        if (email) {
+            const url = `http://localhost:5000/admin/${email}`;
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+
+                    setAdmin(data.isAdmin)
+                    setAdminLoading(false)
+                })
+        }
+
+    }, [user])
+    return [admin, adminLoading]
 
 }
 
